abd59007930e683d1bfcf8a67dce85fce3c87d75
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2026-01-03 18:30:05 +0530

    Add Strip component with admissions marquee to Home
    Introduced a new Strip component displaying an admissions announcement using a marquee. Integrated the Strip component into the Home page above the Banner.

---
+++ src/components/sections/home/Strip.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Strip = () => {
+  return (
+    <div>
+        <div className='flex items-center justify-center h-12 bg-brand-secondary '>
+            <marquee>Admissions Going On From Pre-KG to Grade 11 | For More Information Call: 7540088387,Admissions Going On From Pre-KG to Grade 11 | For More Information Call: 7540088387</marquee>
+        </div>
+    </div>
+  )
+}
+
+export default Strip
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 
 import '../assets/styles/fonts.css'
 import Banner from '../components/sections/home/Banner'
+import Strip from '../components/sections/home/Strip'
 const Home = () => {
   return (
     <div >
       <Banner />
+      <Strip />
     </div>
   )
 }
