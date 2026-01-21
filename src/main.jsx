b378cf16bff6b3ef5c7482e72b12c966507174bd
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2026-01-21 09:52:41 +0530

    Add hydration support for React root rendering
    Switches to using hydrateRoot when the root element has child nodes, enabling server-side rendering hydration. Also updates the build script to run prerendering after building with Vite.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+if (document.getElementById('root').hasChildNodes()) {
+  hydrateRoot(document.getElementById('root'),
+    <StrictMode>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </StrictMode>
+  )
+} else {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}
